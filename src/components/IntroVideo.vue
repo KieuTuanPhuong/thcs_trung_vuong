8a5ec144fa9b792556923a6a8197314e010540c5
KieuTuanPhuong <maingmame23114@gmail.com>
2024-03-23 11:34:34 +0700

    Add Intro video

---
+++ src/components/IntroVideo.vue
@@ -0,0 +1,29 @@
+<script setup>
+
+</script>
+
+<template>
+<div class="video-section">
+    <div class="video-container">
+        <video id="autoplayVideo" controls autoplay muted>
+            <source src="../assets/videos/video3.mp4" type="video/mp4">
+            Your browser does not support the video tag.
+        </video>
+    </div>
+</div>
+</template>
+
+<style scoped>
+.video-section {
+    background-image: url(../assets/images/background/ds1.jpg);
+    padding: 12.5rem 0 6.25rem 0;
+}
+
+.video-container {
+    padding: 2rem 0;
+}
+
+.video-container video {
+    width: 100%;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,13 @@
 <script setup>
 import MainInfomation from "@/components/MainInfomation.vue";
+import IntroVideo from "@/components/IntroVideo.vue";
 import Summarize from "@/components/Summarize.vue";
 </script>
 
 <template>
   <div class="main">
     <MainInfomation />
+    <IntroVideo />
     <Summarize />
   </div>
 </template>
